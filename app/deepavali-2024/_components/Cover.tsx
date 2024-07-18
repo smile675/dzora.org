13c01819bb877e684473dd2fba9e289d5eaab39d
Ismail Hossain <angred.ismail@gmail.com>
2024-07-18 16:08:06 +0800

    initial setup

---
+++ app/deepavali-2024/_components/Cover.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import React from "react";
+import img from "@/public/cover.jpg";
+
+const Cover = () => {
+  return (
+    <div className="relative w-full">
+      <div className="aspect-[3/1]">
+        <Image
+          className="rounded-b-xl"
+          src={img}
+          alt="Cover Image"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+          // quality={100}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Cover;
---
+++ app/deepavali-2024/page.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Cover from "./_components/Cover";
+
+const page = () => {
+  return (
+    <div className="w-full flex justify-center">
+      <div className="w-full max-w-screen-xl h-screen overflow-auto no-scrollbar">
+        <Cover />
+        <p className="my-2 p-4">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
+          veritatis dolorum totam eveniet sed. Facilis totam velit, explicabo
+          inventore reprehenderit animi atque dolor recusandae et quidem quas
+          vitae unde reiciendis error enim debitis laudantium nihil ipsa, cumque
+          saepe maxime officiis. Iste expedita qui quod nostrum laboriosam
+        </p>
+        <div className="w-full sticky top-0 right-0 bottom-0 py-2 px-4 min-h-12 flex justify-between bg-white border border-y-gray-200">
+          <p>ORDER NOW</p>
+          <div>numbers</div>
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+          necessitatibus in earum dolores, recusandae fuga praesentium, aut esse
+          ipsum quos natus ex perspiciatis quidem, atque eius distinctio
+          voluptas consequuntur ullam! Rerum modi nemo quos vero earum suscipit
+          ducimus possimus enim quo aliquid temporibus, alias quae magnam
+          voluptatum eos laborum vitae itaque neque veritatis commodi
+          laudantium? Dolorum nam consequuntur natus minus, possimus quas, quam
+          eos explicabo rerum fuga consectetur, officia sit ullam eius vero
+          vitae velit omnis ducimus aliquid sapiente eligendi? Tempora molestias
+          distinctio officia reprehenderit aliquid repellat hic recusandae
+          perferendis laborum possimus ipsum architecto, voluptates a dolor sunt
+          non voluptatum corrupti cupiditate accusantium impedit aperiam dicta.
+          Voluptates eveniet animi magni repudiandae cupiditate error? Eum
+          similique at dolores quibusdam adipisci nesciunt cum beatae architecto
+          tempore sint perspiciatis, ipsum amet minima facere enim velit
+          voluptate fugiat quis sequi quas quasi. Molestias dolorem architecto
+          iusto dolor, deserunt culpa adipisci laboriosam consectetur magnam
+          tempore quisquam libero, vel impedit officiis corporis rem voluptate
+          maiores minus expedita quasi molestiae fugit laudantium? Vitae iusto
+          non culpa ratione maxime debitis sunt quasi corrupti deleniti ducimus
+          ab assumenda magnam eum dolorem ullam rerum ad magni fugit animi,
+          delectus voluptate, in dignissimos rem. Officiis voluptate iste eos
+          ipsam nesciunt illo! ducimus possimus enim quo aliquid temporibus,
+          alias quae magnam voluptatum eos laborum vitae itaque neque veritatis
+          commodi laudantium? Dolorum nam consequuntur natus minus, possimus
+          quas, quam eos explicabo rerum fuga consectetur, officia sit ullam
+          eius vero vitae velit omnis ducimus aliquid sapiente eligendi? Tempora
+          molestias distinctio officia reprehenderit aliquid repellat hic
+          recusandae perferendis laborum possimus ipsum architecto, voluptates a
+          dolor sunt non voluptatum corrupti cupiditate accusantium impedit
+          aperiam dicta. Voluptates eveniet animi magni repudiandae cupiditate
+          error? Eum similique at dolores quibusdam adipisci nesciunt cum beatae
+          architecto tempore sint perspiciatis, ipsum amet minima facere enim
+          velit voluptate fugiat quis sequi quas quasi. Molestias dolorem
+          architecto iusto dolor, deserunt culpa adipisci laboriosam consectetur
+          magnam tempore quisquam libero, vel impedit officiis corporis rem
+          voluptate maiores minus expedita quasi molestiae fugit laudantium?
+          Vitae iusto non culpa ratione maxime debitis sunt quasi corrupti
+          deleniti ducimus ab assumenda magnam eum dolorem ullam rerum ad magni
+          fugit animi, delectus voluptate, in dignissimos rem. Officiis
+          voluptate iste eos ipsam nesciunt illo!
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
