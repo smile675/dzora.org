51dd449e62f4577f9c20e65c95b08308ed7e896b
Ismail Hossain <angred.ismail@gmail.com>
2024-08-17 15:37:28 +0800

    added. order date

--- app/deepavali-2024/_components/OrderSection.tsx
+++ app/deepavali-2024/_components/OrderSection.tsx
@@ -1,11 +1,27 @@
 "use client";
+import { Clock } from "lucide-react";
 import React from "react";
 import { FaEnvelope, FaWhatsapp } from "react-icons/fa";
 
 const OrderSection = () => {
   return (
     <div className="py-2 px-4 min-h-20  flex justify-between items-center bg-white border border-y-gray-200">
-      <p className="font-bold text-lg lg:text-xl">ORDER NOW</p>
+      <div>
+        <p className="font-bold text-lg lg:text-xl">
+          ORDER NOW{" "}
+          <span className="font-light text-secondary-foreground text-sm">
+            for Deepavali
+          </span>
+        </p>
+        <p className="text-accent-foreground flex gap-2 items-center">
+          <span>
+            <Clock size={15} />
+          </span>
+          <span>
+            Order before 10<sup>th</sup> of Oct, 2024
+          </span>
+        </p>
+      </div>
       <div className="flex justify-end gap-3">
         <div className="flex items-center justify-center gap-1 border border-gray-200 rounded-full p-2">
           <a
